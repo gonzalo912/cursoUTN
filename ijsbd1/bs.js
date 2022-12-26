14b5dfd36d00fdeeba186e4c5564619da624dbc9
gonzalo912 <golmos@fi.uba.ar>
2022-12-26 16:22:18 -0300

    integracion BD 1

--- ijsbd1/bs.js
+++ ijsbd1/bs.js
@@ -1,15 +1,22 @@
 var mysql = require('mysql');
 var util = require('util');
 
+require('dotenv').config();
+
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'gonzalo0912',
+    database: 'bbdd'
 });
 
 pool.query = util.promisify(pool.query);
 
 module.exports = pool;
 
+// connectionLimit: 10,
+// host: process.env.MYSQL_HOST,
+// user: 'gonzalo',
+// password: process.env.MYSQL_PASSWORD,
+// database: process.env.MYSQL_DB_NAME
